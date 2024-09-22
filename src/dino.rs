a7ee1a46d6d397810c48fe21a38916f6e923c897
BoWen Chai <lovebaihezi@gmail.com>
2024-09-22 07:51:13 -0400

    fixup: dino will stay on the left side of the screen

--- src/dino.rs
+++ src/dino.rs
@@ -3,9 +3,9 @@ use bevy::{
     input::ButtonInput,
     log::info,
     math::{Vec2, Vec3},
-    prelude::{default, Commands, Component, KeyCode, Query, Res, Transform, With},
+    prelude::{default, Commands, Component, EventReader, KeyCode, Query, Res, Transform, With},
     sprite::{Sprite, SpriteBundle},
-    window::Window,
+    window::{Window, WindowResized},
 };
 
 #[derive(Component)]
@@ -14,16 +14,34 @@ pub struct Dino;
 const DINO_WIDTH: f32 = 20.0;
 const DINO_SIZE: Vec2 = Vec2::new(DINO_WIDTH, DINO_WIDTH / 0.618);
 
-pub fn setup_dino(mut commands: Commands) {
-    commands.spawn((SpriteBundle {
-        sprite: Sprite {
-            color: Color::srgb(0.05, 0.05, 0.05),
-            custom_size: Some(DINO_SIZE),
+pub fn setup_dino(mut commands: Commands, window: Query<&Window>) {
+    let window = window.single();
+    let width = window.width();
+
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::srgb(0.05, 0.05, 0.05),
+                custom_size: Some(DINO_SIZE),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, DINO_WIDTH, 0.0)),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(0.0, 100.0, 0.0)),
-        ..default()
-    },));
+        Dino,
+    ));
+}
+
+pub fn dino_pos_fix_system(
+    mut query: Query<(&mut Transform, &Sprite), With<Dino>>,
+    mut events: EventReader<WindowResized>,
+) {
+    for e in events.read() {
+        for (mut transform, _sprite) in query.iter_mut() {
+            let window_width = e.width;
+            transform.translation.x = -window_width / 2.0 + DINO_WIDTH / 2.0;
+        }
+    }
 }
 
 /// Dino will jump when user press space, w, Up, k, or left mouse button
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use bevy::prelude::*;
-use dinosaur::{dino_jump_system, setup_camera, setup_dino, setup_ground, update_ground};
+use dinosaur::{
+    dino_jump_system, dino_pos_fix_system, setup_camera, setup_dino, setup_ground, update_ground,
+};
 
 fn main() {
     let exit = App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::srgb(1.0, 1.0, 1.0)))
         .add_systems(Startup, (setup_ground, setup_dino, setup_camera))
-        .add_systems(Update, (update_ground, dino_jump_system))
+        .add_systems(
+            Update,
+            (update_ground, dino_jump_system, dino_pos_fix_system),
+        )
         .run();
     match exit {
         AppExit::Success => {}
