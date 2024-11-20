8185a4799167cf21f7844c10c340660b3c1b8438
BoWen Chai <lovebaihezi@gmail.com>
2024-11-20 14:13:11 +0800

    fixup: migrate to just.ts 3/N

--- just.ts
+++ just.ts
@@ -1,12 +1,15 @@
 #!/usr/bin/env -S deno run --allow-all
 import $ from "@david/dax";
-import { Command } from "@cliffy/command";
+import { Command, EnumType } from "@cliffy/command";
+
+const envEnum = new EnumType(["linux", "windows", "macos", "macos"]);
 
 interface Env {
   binary: string;
 }
 
-async function installDeps() {
+async function installLinuxDeps() {
+  // TODO: check if Ubuntu
   await $`sudo apt-get update`;
   await $`sudo apt-get install --no-install-recommends pkg-config libx11-dev libasound2-dev libudev-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev clang mold libwayland-dev libxkbcommon-dev`;
 }
@@ -23,7 +26,7 @@ async function buildWasm() {
   await $`cargo build --release --target wasm32-unknown-unknown`;
 }
 
-async function prepareWasmPackage(env: Env) {
+async function prepareWasmPackage(env: Env = { binary: "dinosaur" }) {
   await $`wasm-bindgen --out-name ${env.binary} --out-dir wasm --target web target/wasm32-unknown-unknown/release/${env.binary}.wasm`;
   await $`wasm-opt -O wasm/${env.binary}_bg.wasm -o ${env.binary}.wasm`;
   await $`# Compress Wasm using brotli`;
@@ -36,14 +39,30 @@ async function buildRelease() {
   await $`cargo b --release`;
 }
 
-// TODO: Default Run Install Deps and build Release
 // TODO: Migrate All workflow script to this file
 await new Command()
   .name("just")
   .description("Command used to build whole project")
   .version("0.1.0")
+  .type("env", envEnum)
+  .globalOption("--env <level:env>", "Environment to build", {
+    default: "linux",
+  })
   .description("Script for the dinosaur game").action(async () => {
-    await installDeps();
     await buildRelease();
+  })
+  .command("install-linux-deps", "Install dependencies").action(async () => {
+    await installLinuxDeps();
+  })
+  .command("install-wasm-deps", "Install wasm dependencies").action(
+    async () => {
+      await installWasmDeps();
+    },
+  )
+  .command("build-wasm", "Build wasm").action(async () => {
+    await buildWasm();
+  })
+  .command("prepare-wasm-package", "Prepare wasm package").action(async () => {
+    await prepareWasmPackage();
   })
   .parse(Deno.args);
