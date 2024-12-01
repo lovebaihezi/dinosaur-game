7346d0c0b61d7d9cfabbd464a30d0a6bd3e5a8fd
BoWen Chai <lovebaihezi@gmail.com>
2024-12-01 17:11:23 +0800

    fixup: cargo clippy

--- crates/game/src/camera.rs
+++ crates/game/src/camera.rs
@@ -2,5 +2,5 @@ use bevy::prelude::{Camera2d, Commands};
 
 pub fn setup_camera(mut commands: Commands) {
     // Spawn the camera
-    commands.spawn(Camera2d::default());
+    commands.spawn(Camera2d);
 }
